314d76c864fca0c7801a3389fd85d56735f4c44a
liftoach <liftoachgolf@gmail.com>
2026-05-18 02:42:35 +0300

    add auth service

---
+++ finance-app/internal/service/auth_service.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"kr/finance-app/internal/domain"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func (s *Service) Register(ctx context.Context, email, password string) error {
+	hash, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	user := &domain.User{
+		ID:        uuid.New(),
+		Email:     email,
+		Password:  string(hash),
+		CreatedAt: time.Now(),
+	}
+
+	return s.UserRepository.Create(ctx, user)
+}
+
+func (s *Service) Login(ctx context.Context, email, password string) (string, error) {
+	user, err := s.UserRepository.GetByEmail(ctx, email)
+	if err != nil {
+		if errors.Is(err, ErrNotFound) {
+			return "", ErrNotFound
+		}
+		return "", fmt.Errorf("userRepository.GetByEmail: %w", err)
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)) != nil {
+		return "", errors.New("invalid password")
+	}
+	return s.jwt.Generate(user.ID)
+}
--- finance-app/internal/service/service.go
+++ finance-app/internal/service/service.go
@@ -1,8 +1,60 @@
 package service
 
+import (
+	"context"
+	"kr/finance-app/internal/domain"
+	auth "kr/finance-app/pkg/jwt"
+
+	"github.com/google/uuid"
+)
+
 type Service struct {
 	BudgetRepository      BudgetRepository
 	CategoryRepository    CategoryRepository
 	TransactionRepository TransactionRepository
 	UserRepository        UserRepository
+	jwt                   *auth.JWTManager
+}
+
+type BudgetRepository interface {
+	Create(ctx context.Context, b *domain.Budget) error
+
+	GetByID(ctx context.Context, id uuid.UUID) (*domain.Budget, error)
+	GetAll(ctx context.Context) ([]domain.Budget, error)
+	GetByUserID(ctx context.Context, userID uuid.UUID) ([]domain.Budget, error)
+
+	Update(ctx context.Context, b *domain.Budget) error
+	Delete(ctx context.Context, id uuid.UUID) error
+}
+
+type TransactionRepository interface {
+	Create(ctx context.Context, t *domain.Transaction) error
+
+	GetByID(ctx context.Context, id uuid.UUID) (*domain.Transaction, error)
+	GetAll(ctx context.Context) ([]domain.Transaction, error)
+	GetByUserID(ctx context.Context, userID uuid.UUID) ([]domain.Transaction, error)
+
+	Update(ctx context.Context, t *domain.Transaction) error
+	Delete(ctx context.Context, id uuid.UUID) error
+}
+
+type UserRepository interface {
+	Create(ctx context.Context, user *domain.User) error
+
+	GetByID(ctx context.Context, id uuid.UUID) (*domain.User, error)
+	GetAll(ctx context.Context) ([]domain.User, error)
+	GetByEmail(ctx context.Context, email string) (*domain.User, error)
+
+	Update(ctx context.Context, user *domain.User) error
+	Delete(ctx context.Context, id uuid.UUID) error
+}
+
+type CategoryRepository interface {
+	Create(ctx context.Context, c *domain.Category) error
+
+	GetByID(ctx context.Context, id uuid.UUID) (*domain.Category, error)
+	GetAll(ctx context.Context) ([]domain.Category, error)
+
+	Update(ctx context.Context, c *domain.Category) error
+	Delete(ctx context.Context, id uuid.UUID) error
 }
